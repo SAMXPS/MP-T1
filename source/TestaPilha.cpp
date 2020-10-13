0b5e5788973d24c3d4ecb70cc439de742688aea2
Samuel James <samuel.jlbsb@gmail.com>
2020-10-13 14:47:01 -0300

    Rodando o ./mae_and_test.sh no diretório source
    Programa compila e roda, temos a saída: "programa compila e roda com sucesso"

--- source/TestaPilha.cpp
+++ source/TestaPilha.cpp
@@ -1,9 +1,13 @@
 #include "../include/Pilha.hpp"
 #include "../include/PilhaEncadeada.hpp"
 #include "../include/PilhaVetor.hpp"
-
 #define TipoPilha PilhaEncadeada
+
+#include <iostream>
 
 int main() {
     // TODO: Decidir se iremos usar GTEST ou CATCH para fazer os testes.
+    std::cout << "Programa compila e roda com sucesso" << std::endl;
+
+    return 1;
 }
--- source/make_and_test.sh
+++ source/make_and_test.sh
@@ -1,2 +1,5 @@
+mkdir ../bin &>/dev/null
+mkdir ../bin/obj &>/dev/null
+
 make
-../bin/testa_pilha
+../bin/TestaPilha
