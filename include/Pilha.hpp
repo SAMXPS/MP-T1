68cdd12003d2038aceba32d68b5d616bb24c6e5a
Samuel James <samuel.jlbsb@gmail.com>
2020-10-13 14:34:56 -0300

    Criando arquivos necessários para o projeto

---
+++ include/Pilha.hpp
@@ -0,0 +1,6 @@
+#ifndef PILHA_HPP_INCLUDED
+#define PILHA_HPP_INCLUDED
+
+// TODO: Criar header genérico para pilha
+
+#endif //PILHA_HPP_INCLUDED
---
+++ source/TestaPilha.cpp
@@ -0,0 +1,9 @@
+#include "../include/Pilha.hpp"
+#include "../include/PilhaEncadeada.hpp"
+#include "../include/PilhaVetor.hpp"
+
+#define TipoPilha PilhaEncadeada
+
+int main() {
+    // TODO: Decidir se iremos usar GTEST ou CATCH para fazer os testes.
+}
