2b8a26cc55913aeb63e1a31d1e6ee89dfe60a893
Samuel James <samuel.jlbsb@gmail.com>
2020-10-14 14:36:58 -0300

    Header de classe abstrata para Pilha

--- include/Pilha.hpp
+++ include/Pilha.hpp
@@ -1,31 +1,49 @@
 #ifndef PILHA_HPP_INCLUDED
 #define PILHA_HPP_INCLUDED
 
-/*
-Funções a codificar:
-
-Push (pilha, elemento) - coloca elemento no topo da pilha
-Pop (pilha) - retira elemento do topo da pilha – retorna um elemento
-Top (pilha) – retorna elemento do topo da pilha sem modificar a pilha – retorna um
-elemento
-Size (pilha) – retorna int tamanho da pilha
-SetSize (pilha, int) – muda o tamanho da pilha – retorna True se deu certo, False caso
-contrário
-IsFull (pilha) – retorna verdadeiro se a pilha está cheia e falso caso contrário
-IsEmpty (pilha) - retorna verdadeiro se a pilha está vazia e falso caso contrário
-CreateStack () – cria pilha – retorna pilha
-DestroyStack (pilha) – destrói pilha 
-*/
-
 template <class ItemType>
 class Pilha {
     public:
-        Pilha() {};
-
         /**
          * Retorna true se a pilha não contém nenhum elemento.
-        */
-        bool isEmpty();
+         */
+        virtual bool isEmpty() = 0;
+
+        /**
+         * Retorna verdadeiro se a pilha está cheia, falso caso contrário.
+         */
+        virtual bool isFull() = 0;
+
+        /**
+         * Coloca elemento no topo da pilha.
+         * Retorna true em caso de sucesso, false em caso de falha.
+         */
+        virtual bool push (ItemType elemento) = 0;
+
+        /**
+         * Retira elemento do topo da pilha – retorna um elemento.
+         */
+        virtual ItemType* pop() = 0; 
+
+        /**
+         * Retorna elemento do topo da pilha sem modificar a pilha – retorna um elemento.
+         */
+        virtual ItemType* top() = 0;
+
+        /**
+         * Retorna int tamanho da pilha
+         */
+        virtual int elementCount() = 0;
+
+        /**
+         * Retorna int tamanho da pilha
+         */
+        virtual int getCapacity() = 0;
+
+        /**
+         * Muda o tamanho da pilha – retorna True se deu certo, False caso contrário.
+         */
+        virtual bool setCapacity(int) = 0;
 };
 
 #endif //PILHA_HPP_INCLUDED
