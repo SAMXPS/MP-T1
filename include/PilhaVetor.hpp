5fec87fadfc354764b015f404909a9865937d68f
Samuel James <samuel.jlbsb@gmail.com>
2020-10-14 17:48:55 -0300

    (incompleto) Começando a implementar pilha de vetor

--- include/PilhaVetor.hpp
+++ include/PilhaVetor.hpp
@@ -2,19 +2,52 @@
 #define PILHAVETOR_HPP_INCLUDED
 #include "Pilha.hpp"
 
+#define TAMANHO_DEFAULT 10
+
+namespace _PilhaVetor {
+
+    class _base {
+        private:
+            int _elementSize;
+            int _elementCount = 0;
+            int _capacity;
+            void* _data;
+        public:
+            _base(int elementSize, int capacity); 
+            ~_base();
+
+            bool  push(void* data);
+            void* pop();
+            void* top();
+            int   elementCount();
+            int   getCapacity();
+    };
+};
+
 template <class ItemType>
 class PilhaVetor : public Pilha<ItemType> {
+    private:
+        _PilhaVetor::_base* base;
+
     public:
-        PilhaVetor();
+        PilhaVetor() {
+            base = new _PilhaVetor::_base(sizeof(ItemType), TAMANHO_DEFAULT);
+        }
+
         ~PilhaVetor();
 
-        bool isEmpty();
-        bool isFull();
         bool push(ItemType elemento);
+
         ItemType* pop(); 
         ItemType* top();
-        int getCapacity();
-        bool setCapacity(int);
+
+        int getCapacity() {
+            return base->getCapacity();
+        }
+
+        bool setCapacity(int capacity) {
+            return base->setCapacity(capacity);
+        }
 };
 
 #endif //PILHAVETOR_HPP_INCLUDED
