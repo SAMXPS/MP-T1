134a96f0060482bb9de61a477802480e5b77f07e
Samuel James <samuel.jlbsb@gmail.com>
2020-10-18 14:40:47 -0300

    Reorganizando diretório gcov

--- gcov/gcov.sh
+++ gcov/gcov.sh
@@ -2,13 +2,13 @@ rm *.gcov
 cd ../source/
 
 echo "#define TipoPilha PilhaVetor" > TestaPilhaTipo.cpp
-source ../source/make_and_test.sh > ../gcov/make.out
+source ../source/make_and_test.sh > ../gcov/gtest.out
 gcov PilhaVetor.cpp --object-file ../bin/obj/ | grep -A 3 "File 'PilhaVetor.cpp'" > ../gcov/gcov.out
 mv PilhaVetor.cpp.gcov ../gcov/
 rm *.gcov
 
 echo "#define TipoPilha PilhaEncadeada" > TestaPilhaTipo.cpp
-source ../source/make_and_test.sh >> ../gcov/make.out
+source ../source/make_and_test.sh >> ../gcov/gtest.out
 gcov PilhaEncadeada.cpp --object-file ../bin/obj/ | grep -A 3 "File 'PilhaEncadeada.cpp'"  >> ../gcov/gcov.out
 mv PilhaEncadeada.cpp.gcov ../gcov/
 rm *.gcov
